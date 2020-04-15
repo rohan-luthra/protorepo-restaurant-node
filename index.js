76c11edabf68c75cef1cf8937b89be0c09cd7d3a
unknown <rohanluthra.1123@gmail.com>
2020-04-15 19:04:51 +0530

    fixes

---
+++ index.js
@@ -0,0 +1,11 @@
+const RestaurantGRPC = require("./restaurant_grpc_pb");
+const TaxGRPC = require("./tax_grpc_pb");
+const RestaurantGroupGRPC = require("./restaurant_group_grpc_pb");
+const CurrencyGRPC = require('./currency_grpc_pb');
+
+module.exports = {
+    RestaurantGroupGRPC,
+    RestaurantGRPC,
+    TaxGRPC,
+    CurrencyGRPC
+}
